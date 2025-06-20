e266f6e6fdd2ae989f31fff5ab9fc135e5cb9eea
AkshayAshok2 <shay2shok@gmail.com>
2025-06-20 14:07:28 -0400

    updating with less old code and reformatting to align with Google C++ style guide

--- client/client.cc
+++ client/client.cc
@@ -1,81 +1,224 @@
-#include <stdio.h>
-#include <sys.socket.h>
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <openssl/evp.h>
 
-#define RCVBUFSIZE 512		    /* The receive buffer size */
-#define SNDBUFSIZE 512		    /* The send buffer size */
+// Constants
+#define RECEIVE_BUFFER_SIZE 512
+#define SEND_BUFFER_SIZE 512
 #define MDLEN 32
+#define DATA_PATH "./files"
+#define FILE_READ_BUFFER_SIZE 1024
+#define SERVER_PORT 9090
 
-void fatal_error(char *message) {
-  perror(message);
+// Struct definitions
+struct ListMessageResponse {
+    uint8_t filename_bytes;
+    char filename[RECEIVE_BUFFER_SIZE];
+    uint8_t file_hash_bytes;
+    char file_hash[RECEIVE_BUFFER_SIZE];
+};
+
+struct PullMessageResponse {
+    uint8_t file_hash_bytes;
+    char file_hash[RECEIVE_BUFFER_SIZE];
+    uint32_t file_contents_bytes;
+    char file_contents[RECEIVE_BUFFER_SIZE];
+};
+
+struct DiffMessage {
+    uint8_t filename_bytes;
+    char filename[RECEIVE_BUFFER_SIZE];
+    uint8_t file_hash_bytes;
+    char file_hash[RECEIVE_BUFFER_SIZE];
+};
+
+// Function prototypes
+void FatalError(std::string message);
+ListMessageResponse *List(int client_socket, uint8_t *server_file_count, char *receive_buffer);
+ListMessageResponse *GetFileNamesAndHashes(uint8_t *file_count);
+char *CalculateSha256Hash(const char *file_path);
+DiffMessage *Diff(ListMessageResponse *server_files, uint8_t server_file_count,
+                  ListMessageResponse **client_files, uint8_t cilent_file_count,
+                  uint8_t diff_file_count, uint8_t suppress_output);
+void ReceiveFileWithHash(int client_socket, const char *filename);
+void Pull(int client_socket, uint8_t *diff_file_count, DiffMessage *diff_files, char *receive_buffer);
+
+// Main function
+int main(int argc, char *argv[]) {
+  int client_socket;
+  int message_length;
+  struct sockaddr_in server_address;
+  struct sockaddr_in client_address;
+
+  char send_buffer[SEND_BUFFER_SIZE];
+  char receive_buffer[RECEIVE_BUFFER_SIZE];
+    
+  int i;
+  int server_port = 8080;
+  char *server_ip_address = "127.0.0.1";
+
+  uint8_t server_file_count;
+  uint8_t client_file_count;
+  uint8_t diff_file_count;
+  bool is_diff = true;
+
+  ListMessageResponse *server_files;
+  ListMessageResponse *client_files;
+  DiffMessage *diff_files;
+
+  // Create a new TCP socket
+  if ((client_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+    FatalError("socket() failed");
+
+  // Construct the server address structure
+  memset(&server_address, 0, sizeof(server_address));
+  server_address.sin_family        = AF_INET;
+  server_address.sin_port          = htons(server_port);
+  server_address.sin_addr.s_addr   = inet_addr(server_ip_address);
+
+  // Establish connection to the server
+  if (connect(client_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
+    FatalError("connect() failed");
+
+  std::cout << "Welcome to MyMusic!" << std::endl;
+
+  while (true) {
+    std::cout << "\nSelect an option:\n1. LIST\n2. DIFF\n3. PULL\n4. LEAVE" << std::endl;
+    uint8_t option;
+    std::cin >> option;
+
+    switch (option) {
+      case 1: // LIST
+        if (server_files != nullptr) {
+          delete[] server_files;
+        }
+
+        server_files = List(client_socket, &server_file_count, receive_buffer);
+
+        if (server_files == nullptr) {
+          FatalError("`server_files` is null after LIST operation. Exiting.");
+          break;
+        } else if (server_file_count == 0) {
+          FatalError("`server_file_count` is 0 after LIST operation. Exiting.");
+          break;
+        } 
+
+        // Print the received files
+        for (int i = 0; i < server_file_count; i++) {
+          std::cout << "File: " << server_files[i].filename << std::endl;
+          std::cout << "Hash: " << server_files[i].file_hash << std::endl;
+        }
+        
+        break;
+      case 2: // DIFF
+        if (server_file_count == 0 || server_files == nullptr) {
+          std::cout << "LIST has not been called yet. Please call that first." << std::endl;
+          continue;
+        }
+
+        if (client_files != nullptr) {
+          delete[] client_files;
+        }
+
+        diff_files = Diff(server_files, server_file_count, 
+            &client_files, client_file_count, 
+                          diff_file_count, 0);
+        
+        if (client_files == nullptr) {
+          FatalError("`client_files` is null after DIFF operation. Exiting.");
+        }
+        
+        break;
+      case 3: // PULL
+        if (client_files == nullptr) {
+          std::cout << "DIFF has not been called yet. Please call that first." << std::endl;
+          continue;
+        }
+
+        if (is_diff) {
+          Pull(client_socket, &diff_file_count, diff_files, receive_buffer);
+        } else {
+          std::cout << "No files to pull." << std::endl;
+        }
+
+        break;
+      case 4: // LEAVE
+        if (send(client_socket, &option, sizeof(option), 0) != sizeof(option)) {
+          FatalError("`send()` sent unexpected number of bytes.");
+        }
+
+        if (server_files != nullptr) {
+          delete[] server_files;
+        }
+        if (client_files != nullptr) {
+          delete[] client_files;
+        }
+        if (diff_files != nullptr) {
+          delete[] diff_files;
+        }
+
+        close(client_socket);
+        return 0;
+      default:
+        std::cout << "Invalid option. Please try again." << std::endl;
+    }
+  }
+
+  return 0;
+}
+
+void FatalError(std::string message) {
+  perror(message.c_str());
   exit(1);
 }
 
-int main(int argc, char *argv[]) {
-  int clientSock;		    /* socket descriptor */
-  int msgLen;
-  struct sockaddr_in serv_addr;   /* The server address */
-  struct sockaddr_in client_addr; /* The client address */
+ListMessageResponse *List(int client_socket, uint8_t *server_file_count, char *receive_buffer) {
+  if (receive_buffer != nullptr) {
+    delete[] receive_buffer;
+  }
 
-  char *studentName;		    /* Your Name */
-  char sndBuf[SNDBUFSIZE];	    /* Send Buffer */
-  char rcvBuf[RCVBUFSIZE];	    /* Receive Buffer */
+  uint8_t option = 1;
+
+  if (send(client_socket, &option, sizeof(option), 0) != sizeof(option)) {
+    FatalError("`send()` sent unexpected number of bytes.");
+  }
+
+  uint8_t bytes_received = recv(client_socket, server_file_count, sizeof(*server_file_count), 0);
+
+  if (bytes_received < 0) {
+    FatalError("First `recv()` failed.");
+  } else if (bytes_received == 0) {
+    FatalError("Server closed connection during LIST operation.");
+  }
+
+  uint32_t total_message_size = *server_file_count *sizeof(uint8_t) + 2 * RECEIVE_BUFFER_SIZE;
+
+  if (receive_buffer != nullptr) {
+    FatalError("`malloc` failed to allocate memory for `receive_buffer`.");
+  }
+
+  memset(receive_buffer, 0, total_message_size);
+  int total_bytes_received = 0;
+
+  while (total_bytes_received < total_message_size) {
+    bytes_received = recv(client_socket, receive_buffer + total_bytes_received,
+                          total_message_size - total_bytes_received, 0);
     
-  int i;			    /* Counter Value */
-  int servPort = 8080;
-  char *servIP = "127.0.0.1";
-
-  /* Get the Student Name from the command line */
-  if (argc != 2) 
-  {
-    printf("Incorrect input format. The correct format is:\n\tnameChanger your_name\n");
-        exit(1);
-    }
-    studentName = argv[1];
-    memset(&sndBuf, 0, RCVBUFSIZE);
-    memset(&rcvBuf, 0, RCVBUFSIZE);
-
-    msgLen = strlen(studentName);
-    strncpy(sndBuf, studentName, msgLen);
-    sndBuf[msgLen] = '\0';
-
-    /* Create a new TCP socket*/
-    if ((clientSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-        fatal_error("socket() failed");
-
-    /* Construct the server address structure */
-    memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family        = AF_INET;
-    serv_addr.sin_port          = htons(servPort);
-    serv_addr.sin_addr.s_addr   = inet_addr(servIP);
-
-    /* Establish connection to the server */
-    if (connect(clientSock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        fatal_error("connect() failed");
-    
-    /* Send the string to the server */
-    if (send(clientSock, sndBuf, msgLen, 0) != msgLen)
-        fatal_error("send() sent unexpected number of bytes");
-
-    /* Receive and print response from the server */
-    int receivedBytes = 1;
-    while (receivedBytes > 0) {
-        if ((receivedBytes = recv(clientSock, rcvBuf, RCVBUFSIZE - 1, 0)) < 0) {
-            fatal_error("recv() failed");
-        }
+    if (bytes_received < 0) {
+      FatalError("`recv()` failed during LIST operation for file names and hashes.");
+    } else if (bytes_received == 0) {
+      FatalError("Server closed connection during LIST operation. Stopping here.");
+      break;
     }
 
-    close(clientSock);
-    rcvBuf[receivedBytes] = '\0';
+    total_bytes_received += bytes_received;
+  }
 
-    printf("%s\n", studentName);
-    printf("Transformed input is: ");
-    for(i = 0; i < MDLEN; i++) printf("%02x", rcvBuf[i]);
-    printf("\n");
+  ListMessageResponse *server_files = (ListMessageResponse *)malloc(*server_file_count * sizeof(ListMessageResponse));
 
-    return 0;
 }
--- server/server.cc
+++ server/server.cc
@@ -6,9 +6,9 @@
 #include <string>
 #include <openssl/evp.h>
 
-#define RCVBUFSIZE 512		/* The receive buffer size */
-#define SNDBUFSIZE 512		/* The send buffer size */
-#define BUFSIZE 40		/* Your name can be as many as 40 chars*/
+#define RECEIVE_BUFFER_SIZE 512 // The receive buffer size
+#define SEND_BUFFER_SIZE 512    // The send buffer size
+#define BUFFER_SIZE 40		    // Your name can be as many as 40 chars
 
 void fatal_error(char *message)
 {
@@ -18,71 +18,71 @@ void fatal_error(char *message)
 
 int main(int argc, char *argv[])
 {
-  int serverSock;				    /* Server Socket */
-  int clientSock;				/* Client Socket */
-  struct sockaddr_in changeServAddr;		/* Local address */
-  struct sockaddr_in changeClntAddr;		/* Client address */
-  unsigned short changeClientPort;    /* Client port */
-  unsigned short changeServPort;		/* Server port */
-  unsigned int clntLen;			/* Length of address data struct */
+  int server_socket;
+  int client_socket;
+  struct sockaddr_in change_server_address;
+  struct sockaddr_in change_client_address;
+  unsigned short change_client_port;
+  unsigned short change_server_port;
+  unsigned int client_len;
 
-  char nameBuf[BUFSIZE];			/* Buff to store name from client */
-  unsigned char md_value[EVP_MAX_MD_SIZE];	/* Buff to store change result */
-  EVP_MD_CTX *mdctx;				/* Digest data structure declaration */
-  const EVP_MD *md;				/* Digest data structure declaration */
-  int md_len;					/* Digest data structure size tracking */
+  char name_buf[BUFFER_SIZE];               // Buffer to store name from client
+  unsigned char md_value[EVP_MAX_MD_SIZE];	// Buff to store change result
+  EVP_MD_CTX *mdctx;				        // Digest data structure declaration
+  const EVP_MD *md;				            // Digest data structure declaration
+  int md_len;					            // Digest data structure size tracking
 
-  changeServPort = 9090;
+  change_server_port = 9090;
 
-  /* Create new TCP Socket for incoming requests*/
-  if ((serverSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+  // Create new TCP Socket for incoming requests
+  if ((server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
     fatal_error("socket() failed");
 
-  /* Construct local address structure*/
-  memset(&changeServAddr, 0, sizeof(changeServAddr));
-  changeServAddr.sin_family       = AF_INET;
-  changeServAddr.sin_addr.s_addr  = htonl(INADDR_ANY);
-  changeServAddr.sin_port         = htons(changeServPort);
+  // Construct local address structure
+  memset(&change_server_address, 0, sizeof(change_server_address));
+  change_server_address.sin_family       = AF_INET;
+  change_server_address.sin_addr.s_addr  = htonl(INADDR_ANY);
+  change_server_address.sin_port         = htons(change_server_port);
   
-  /* Bind to local address structure */
-  if (bind(serverSock, (struct sockaddr *) &changeServAddr, sizeof(changeServAddr)) < 0)
+  // Bind to local address structure
+  if (bind(server_socket, (struct sockaddr *) &change_server_address, sizeof(change_server_address)) < 0)
     fatal_error("bind() failed");
 
-  /* Listen for incoming connections */
-  if (listen(serverSock, 5) < 0)
+  // Listen for incoming connections
+  if (listen(server_socket, 5) < 0)
     fatal_error("listen() failed");
 
-  /* Loop server forever*/
+  // Loop server forever
   while(1)
   {
-    /* Accept incoming connection */
-    clntLen = sizeof(changeClntAddr);
-    if ((clientSock = accept(serverSock, (struct sockaddr *) &changeClntAddr, &clntLen)) < 0)
+    // Accept incoming connection
+    client_len = sizeof(change_client_address);
+    if ((client_socket = accept(server_socket, (struct sockaddr *) &change_client_address, &client_len)) < 0)
       fatal_error("accept() failed");
 
-    /* Extract Your Name from the packet, store in nameBuf */
-    int bytesReceived;
-    if ((bytesReceived = recv(clientSock, nameBuf, RCVBUFSIZE, 0)) < 0)
+    // Extract Your Name from the packet, store in name_buf
+    int bytes_received;
+    if ((bytes_received = recv(client_socket, name_buf, RECEIVE_BUFFER_SIZE, 0)) < 0)
       fatal_error("recv() failed");
 
-    /* Run this and return the final value in md_value to client */
-    /* Takes the client name and changes it */
-    /* Students should NOT touch this code */
+    // Run this and return the final value in md_value to client
+    // Takes the client name and changes it
+    // Students should NOT touch this code
     OpenSSL_add_all_digests();
     md = EVP_get_digestbyname("SHA256");
     mdctx = EVP_MD_CTX_create();
     EVP_DigestInit_ex(mdctx, md, NULL);
-    EVP_DigestUpdate(mdctx, nameBuf, strlen(nameBuf));
+    EVP_DigestUpdate(mdctx, name_buf, strlen(name_buf));
     EVP_DigestFinal_ex(mdctx, md_value, &md_len);
     EVP_MD_CTX_destroy(mdctx);
 
-    /* Return md_value to client */
-    if (send(clientSock, md_value, sizeof(md_value), 0) < 0)
+    // Return md_value to client */
+    if (send(client_socket, md_value, sizeof(md_value), 0) < 0)
       fatal_error("send() failed");
 
-    close(clientSock);
+    close(client_socket);
   }
 
-  close(serverSock);
+  close(server_socket);
   return 0;
 }
